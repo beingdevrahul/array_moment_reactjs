f9bf164f45611d8b22b7576c393901b0cfdb3570
rahul sharma <rahul.sharma@monkhub.com>
2022-09-28 14:57:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Cards from './Componet/Cards';
+import Header from './Componet/Header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Cards/>
+    </>
+  );
+}
+
+export default App;
